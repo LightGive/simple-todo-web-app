8a6ab556cce707fc1ce2c7578652709f7d13709f
LightGive <roi.akase@outlook.jp>
2026-03-31 22:42:48 +0900

    新規プロジェクトで追加されたファイルを追加

---
+++ simple-todo-web-app/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace simple_todo_web_app.Data
+{
+	public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+	{
+	}
+}
