e4075d9d82251a8648984d4a8fccdcea796a4ff2
LightGive <roi.akase@outlook.jp>
2026-04-02 09:35:03 +0900

    エラー時に表示するページを作成

---
+++ simple-todo-web-app/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace simple_todo_web_app.Controllers
+{
+	public class ErrorController : Controller
+	{
+		[HttpGet("/error/service-unavailable")]
+		public IActionResult ServiceUnavailable()
+		{
+			return View();
+		}
+	}
+}
