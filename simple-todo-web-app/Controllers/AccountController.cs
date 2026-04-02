e1a15f64a4e45af50d758fac31ad680a9f9df984
LightGive <roi.akase@outlook.jp>
2026-04-02 17:41:54 +0900

    ログイン画面を仮実装

---
+++ simple-todo-web-app/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace simple_todo_web_app.Controllers
+{
+	public class AccountController : Controller
+	{
+		[HttpGet("/account/login")]
+		public IActionResult Login()
+		{
+			return View("Login");
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Login(LoginViewModel model)
+		{
+			// 入力チェック
+			if (!ModelState.IsValid)
+			{
+				var displayMessage = "";
+				foreach (var state in ModelState)
+				{
+					var key = state.Key;
+					foreach (var error in state.Value.Errors)
+					{
+						displayMessage += $"{error.ErrorMessage}";
+						Console.WriteLine($"ValidationError: {key}: {error.ErrorMessage}");
+					}
+				}
+
+				return View(model);
+			}
+
+			Console.WriteLine($"Email: {model.Email}, Password: {model.Password}");
+
+			// ASP.NetIdentityを使用してユーザー認証
+			// 認証失敗時は以下でエラーを追加する
+			// ModelState.AddModelError(string.Empty, "メールアドレスまたはパスワードが正しくありません。");
+
+			return View(model);
+		}
+	}
+	public class LoginViewModel
+	{
+		[Required(ErrorMessage = "メールアドレスを入力してください。")]
+		[EmailAddress(ErrorMessage = "メールアドレスの形式が正しくありません。")]
+		public string Email { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "パスワードを入力してください。")]
+		public string Password { get; set; } = string.Empty;
+	}
+}
