b746ebf9172481c5aad7415499b16880083a93aa
LightGive <roi.akase@outlook.jp>
2026-04-03 13:40:10 +0900

    AspNetUsersを拡張

---
+++ simple-todo-web-app/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using simple_todo_web_app.Common.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace simple_todo_web_app.Models
+{
+	public class ApplicationUser: IdentityUser
+	{
+		[Required]
+		[MaxLength(CharacterConstants.NameMaxLength)]
+		public string DisplayName { get; private set; } = string.Empty;
+	}
+}
