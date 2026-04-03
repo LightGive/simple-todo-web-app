d0111a408ae0bd7fa7256c3277623ad54ad2350d
LightGive <roi.akase@outlook.jp>
2026-04-03 11:21:11 +0900

    パスワード規則を実装

--- simple-todo-web-app/Models/RegisterViewModel.cs
+++ simple-todo-web-app/Models/RegisterViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using simple_todo_web_app.Attributes;
+using simple_todo_web_app.Common;
+using System.ComponentModel.DataAnnotations;
 
 namespace simple_todo_web_app.Models
 {
@@ -7,8 +9,12 @@ namespace simple_todo_web_app.Models
 		[Required(ErrorMessage = "メールアドレスを入力してください。")]
 		[EmailAddress(ErrorMessage = "メールアドレスの形式が正しくありません。")]
 		public string Email { get; set; } = string.Empty;
+
 		[Required(ErrorMessage = "パスワードを入力してください。")]
+		[MinLength(PasswordPolicy.RequiredLength, ErrorMessage = "パスワードの長さは{1}文字以上にして下さい")]
+		[RegularExpression(@"^(?=.*[a-z])(?=.*\d).*$",ErrorMessage = "パスワードは英小文字と数字をそれぞれ1文字以上含めてください")]
 		public string Password { get; set; } = string.Empty;
+
 		[Required(ErrorMessage = "パスワード（確認）を入力してください。")]
 		[Compare("Password", ErrorMessage = "パスワードと確認用パスワードが一致しません。")]
 		public string ConfirmPassword { get; set; } = string.Empty;
