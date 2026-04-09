e324ead010a4649aee84ccbe412781a36794be94
LightGive <roi.akase@outlook.jp>
2026-04-09 14:18:23 +0900

    UtcからJstに変換する共通メソッドを追加

---
+++ simple-todo-web-app/Common/Utilities/DateTimeUtility.cs
@@ -0,0 +1,17 @@
+﻿namespace simple_todo_web_app.Common.Utilities
+{
+	public class DateTimeUtility
+	{
+		static readonly TimeZoneInfo JstTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
+
+		public static DateTime UtcToJst(DateTime utcDateTime)
+		{
+			return TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, JstTimeZone);
+		}
+
+		public static DateOnly UtcToJstDate(DateTime utcDateTime)
+		{
+			return DateOnly.FromDateTime(UtcToJst(utcDateTime));
+		}
+	}
+}
