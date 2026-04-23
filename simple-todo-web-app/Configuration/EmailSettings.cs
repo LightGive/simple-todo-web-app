c2aad36420b925767555df9868976c735bfc26c6
LightGive <roi.akase@outlook.jp>
2026-04-23 13:44:14 +0900

    appsettings.jsonから読み込むためのクラスを作成

---
+++ simple-todo-web-app/Configuration/EmailSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace simple_todo_web_app.Configuration
+{
+	/// <summary>
+	/// appsettings.jsonのEmailSettingsセクションに対応するクラス
+	/// </summary>
+	public class EmailSettings
+	{
+		public string SmtpServer { get; set; } = "";
+		public int Port { get; set; }
+		public string SenderName { get; set; } = "";
+		public string SenderEmail { get; set; } = "";
+		public string Password { get; set; } = "";
+	}
+}
